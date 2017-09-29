86e51b09e3a09b9c0e0f57e09112b11c0f220c87
cmorda <cmorda@gmail.com>
2017-09-29 09:41:12 -0700

    REMOVE unload light etc
    possibly the final code. removed unload light as this is being offloaded to the Pi. removed code for different video tracks. everything is now 45 second video.

--- queueTimer.ino
+++ queueTimer.ino
@@ -7,28 +7,16 @@ const int load1pin = 2;// the number of the LED pin
 const int load2pin = 3;
 const int load3pin = 4;
 
-const int unload1pin = 5;
-const int unload2pin = 6;
-const int unload3pin = 7;
+const int vid1pin = 5;
+const int vid2pin = 6;
+const int vid3pin = 7;
 
-const int vid130pin = 8;
-const int vid145pin = 9;
-const int vid160pin = 10;
-
-const int vid230pin = 11;
-const int vid245pin = 12;
-const int vid260pin = 13;
-
-const int vid330pin = 14;
-const int vid345pin = 15;
-const int vid360pin = 16;
-
-const int sel45pin = 17;
-const int sel60pin = 18;
+const int sel60pin = 17;
+const int sel75pin = 18;
 
 // Variables will change:
-int sel45 = 0;
 int sel60 = 0;
+int sel75 = 0;
 int blink = 0;
 
 
@@ -42,163 +30,109 @@ void setup() {
   Serial.println("Setting up pins");
   // set the digital pin as output:
   int outMin = 2;
-  int outMax = 16;
+  int outMax = 7;
   for (int o = outMin; o <= outMax; o++)
   {
     pinMode(o, OUTPUT);
     digitalWrite(o, HIGH);
   }
-  pinMode(sel45pin, INPUT_PULLUP);
   pinMode(sel60pin, INPUT_PULLUP);
+  pinMode(sel75pin, INPUT_PULLUP);
 }
 
 // functions
-  int pad() {
-    sel45 = digitalRead(sel45pin);
-    sel60 = digitalRead(sel60pin);
-    Serial.println("Polling time selection - pin17 = 45s | pin18 = 60s");
-    if (sel45 == LOW) {
-      return 15000;
-      Serial.println("45sec selected");
-    }
-    else if (sel60 == LOW) {
-      return 30000;
-      Serial.println("60sec selected");
-    }
-    else {
-      return 0;
-      Serial.println("30sec selected");
-    }
+int pad() {
+  sel60 = digitalRead(sel60pin);
+  sel75 = digitalRead(sel75pin);
+  Serial.println("Polling time selection - pin17 = 60s | pin18 = 75s");
+  if (sel60 == LOW) {
+    return 15000;
+    Serial.println("60sec selected");
   }
-void loop() {
-  // here is where you'd put code that needs to be running all the time.
-  
-  pad();
-  // 0 SECOND POSITION
-  
-  Serial.println("turn ON LOAD-1 | OFF UNLOAD-1");
-  digitalWrite(load1pin, LOW);
-  digitalWrite(unload1pin, HIGH);
-  if (sel45 == LOW){
-    digitalWrite(vid145pin, LOW);
-    Serial.println("Start vid145");
-  }
-  else if (sel60 == LOW){
-    digitalWrite(vid160pin, LOW);
-    Serial.println("Start vid160");
+  else if (sel75 == LOW) {
+    return 30000;
+    Serial.println("75sec selected");
   }
   else {
-    digitalWrite(vid130pin, LOW);
-    Serial.println("Start vid130");
+    return 0;
+    Serial.println("45sec selected");
   }
+}
+void loop() {
+  // 0 second position
+
+  // read pad variable into memory
+  int padT = pad();
+  // insert padding as necessary for selected time
+  Serial.print("PAD delay ");
+  Serial.print(padT / 1000);
+  Serial.println("s");
+  delay(padT);
+
+  Serial.println("turn ON LOAD-1 | ON Vid-1");
+  digitalWrite(load1pin, LOW);
+  digitalWrite(vid1pin, LOW);
+
   Serial.println("delay 15s");
   delay(15000);
 
   // 15 SECOND POSITION
-  
-  Serial.println("turn OFF LOAD-1 | ON UNLOAD-2 | OFF vid1");
+
+  Serial.println("turn OFF LOAD-1 | OFF Vid-1");
   digitalWrite(load1pin, HIGH);
-  digitalWrite(unload2pin, LOW);
-  digitalWrite(vid130pin, HIGH);
-  digitalWrite(vid145pin, HIGH);
-  digitalWrite(vid160pin, HIGH);
+  digitalWrite(vid1pin, HIGH);
 
-  Serial.print("delay15s + ");
-  Serial.print(pad());
-  Serial.println("ms");
-  //delay(15000);
+  Serial.println("delay 30s");
+  delay(30000);
 
-  // 3 SECONDS OF FLASHING THE DOOR LIGHTS
+  // insert padding as necessary for selected time
+  Serial.print("PAD delay ");
+  Serial.print(padT / 1000);
+  Serial.println("s");
+  delay(padT);
+
+  // 45 SECOND POSITION
   
-  blink = 0;
-  while(blink < 15){
-    digitalWrite(unload2pin, LOW);
-    delay(100);
-    digitalWrite(unload2pin, HIGH);
-    delay(100);
-    blink++;
-  }
-  delay(27000);
-  delay(pad());
-
-  Serial.println("turn ON LOAD-2 | OFF UNLOAD-2");
+  Serial.println("turn ON LOAD-2 | ON Vid-2");
   digitalWrite(load2pin, LOW);
-  digitalWrite(unload2pin, HIGH);
-  if (sel45 == LOW){
-    digitalWrite(vid245pin, LOW);
-    Serial.println("Start vid245");
-  }
-  else if (sel60 == LOW){
-    digitalWrite(vid260pin, LOW);
-    Serial.println("Start vid260");
-  }
-  else {
-    digitalWrite(vid230pin, LOW);
-    Serial.println("Start vid230");
-  }
+  digitalWrite(vid2pin, LOW);
 
   Serial.println("delay 15s");
   delay(15000);
 
-  Serial.println("turn OFF LOAD-2 | ON UNLOAD-3 | OFF vid2");
+  // 60 SECOND POSITION
+
+  Serial.println("turn OFF LOAD-2 | OFF vid2");
   digitalWrite(load2pin, HIGH);
-  digitalWrite(unload3pin, LOW);
-  digitalWrite(vid230pin, HIGH);
-  digitalWrite(vid245pin, HIGH);
-  digitalWrite(vid260pin, HIGH);
+  digitalWrite(vid2pin, HIGH);
 
-  Serial.print("delay15s + ");
-  Serial.print(pad());
-  Serial.println("ms");
-  //delay(15000);
-  blink = 0;
-  while(blink < 75){
-    digitalWrite(unload3pin, LOW);
-    delay(100);
-    digitalWrite(unload3pin, HIGH);
-    delay(100);
-    blink++;
-  }
-  delay(pad());
+  Serial.println("delay 30s");
+  delay(30000);
 
-  Serial.println("turn ON LOAD-3 | OFF UNLOAD-3");
+  // insert padding as necessary for selected time
+  Serial.print("PAD delay ");
+  Serial.print(padT / 1000);
+  Serial.println("s");
+  delay(padT);
+
+  // 90 SECOND POSITION
+
+  Serial.println("turn ON LOAD-3 | ON Vid-3");
   digitalWrite(load3pin, LOW);
-  digitalWrite(unload3pin, HIGH);
-  if (sel45 == LOW){
-    digitalWrite(vid345pin, LOW);
-    Serial.println("Start vid345");
-  }
-  else if (sel60 == LOW){
-    digitalWrite(vid360pin, LOW);
-    Serial.println("Start vid360");
-  }
-  else {
-    digitalWrite(vid330pin, LOW);
-    Serial.println("Start vid330");
-  }
+  digitalWrite(vid3pin, LOW);
 
   Serial.println("delay 15s");
   delay(15000);
 
-  Serial.println("turn OFF LOAD-3 | ON UNLOAD-1 | OFF vid3");
-  digitalWrite(load3pin, HIGH);
-  digitalWrite(unload1pin, LOW);
-  digitalWrite(vid330pin, HIGH);
-  digitalWrite(vid345pin, HIGH);
-  digitalWrite(vid360pin, HIGH);
+  // 105 SECOND POSITION
 
-  Serial.print("delay15s + ");
-  Serial.print(pad());
-  Serial.println("ms");
-  //delay(15000);
-  blink = 0;
-  while(blink < 75){
-    digitalWrite(unload1pin, LOW);
-    delay(100);
-    digitalWrite(unload1pin, HIGH);
-    delay(100);
-    blink++;
-  }
-  delay(pad());
+  Serial.println("turn OFF LOAD-3 | OFF Vid-3");
+  digitalWrite(load3pin, HIGH);
+  digitalWrite(vid3pin, HIGH);
+
+  Serial.println("delay 30s");
+  delay(30000);
+
+  // BACK TO 0 OR PAD
 
 }
